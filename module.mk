a4ed06e66819aebca61c6517cd61ce968e9f6262
Aurélien Croc <aurelien@ap2c.org>
2008-02-09 02:46:53 +0000

    Switching from SpliX V.1 to SpliX V.2
    git-svn-id: https://svn.code.sf.net/p/splix/code/splix@176 10358e0b-e31b-0410-b13d-bf5c54612e97

---
+++ module.mk
@@ -0,0 +1,44 @@
+#
+#	module.mk			(C) 2007-2008, Aurélien Croc (AP²C)
+#
+#  Compilation file for SpliX
+#
+# Options: DISABLE_JBIG
+# 	   DISABLE_THREADS
+#          DISABLE_BLACKOPTIM
+
+MODE			:= debug
+
+SUBDIRS 		+= src
+TARGETS			:= rastertoqpdl pstoqpdl
+
+
+# Flags
+CXXFLAGS		+= `cups-config --cflags` -Iinclude -Wall
+CXXFLAGS		+= -DTHREADS=2 -DCACHESIZE=2
+DEBUG_CXXFLAGS		+= -DDEBUG  -DDUMP_CACHE
+OPTIMIZED_CXXFLAGS 	+= -g
+OPTIMIZED_CXXFLAGS 	+= -g 
+rastertoqpdl_LDFLAGS	:= `cups-config --ldflags`
+rastertoqpdl_LIBS	:= `cups-config --libs` -lcupsimage
+pstoqpdl_LDFLAGS	:= `cups-config --ldflags`
+pstoqpdl_LIBS		:= `cups-config --libs` -lcupsimage
+ifndef $(DISABLE_JBIG)
+rastertoqpdl_LIBS	+= -ljbig
+endif
+
+
+# Get some information
+CUPSFILTER		:= `cups-config --serverbin`/filter
+ifeq ($(ARCHI),Darwin)
+PSTORASTER		:= pstocupsraster
+else
+PSTORASTER		:= pstoraster
+endif
+
+
+# Specific information needed by pstoqpdl
+src_pstoqpdl_cpp_FLAGS	:= -DRASTERDIR=\"$(CUPSFILTER)\"
+src_pstoqpdl_cpp_FLAGS	+= -DRASTERTOQPDL=\"rastertoqpdl\"
+src_pstoqpdl_cpp_FLAGS	+= -DPSTORASTER=\"$(PSTORASTER)\"
+
