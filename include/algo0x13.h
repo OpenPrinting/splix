d00ccba9d0603e7bb6d4999fdc3bd382d456a760
Aurélien Croc <aurelien@ap2c.org>
2008-02-09 02:02:13 +0000

    Add the Algo 0x13 header file
    git-svn-id: https://svn.code.sf.net/p/splix/code/branches/branche-2.0@158 10358e0b-e31b-0410-b13d-bf5c54612e97

---
+++ include/algo0x13.h
@@ -0,0 +1,74 @@
+/*
+ * 	    algo0x13.h                (C) 2007-2008, Aurélien Croc (AP²C)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the
+ *  Free Software Foundation, Inc.,
+ *  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *  $Id$
+ * 
+ */
+#ifndef _ALGO0X13_H_
+#define _ALGO0X13_H_
+
+#ifndef DISABLE_JBIG
+
+#include "algorithm.h"
+extern "C" {
+#   include "jbig.h"
+}
+
+/**
+  * @brief This class implements the compression algorithm 0x13.
+  */
+class Algo0x13 : public Algorithm
+{
+    protected:
+        typedef struct bandList_s {
+            BandPlane*          band;
+            struct bandList_s*  next;
+        } bandList_t;
+
+        typedef struct info_s {
+            bandList_t**        list;
+            bandList_t*         last;
+            unsigned char*      data;
+            unsigned long       size;
+            unsigned long       maxSize;
+        } info_t;
+
+    protected:
+        bool                    _compressed;
+        jbg_enc_state           _state;
+        bandList_t*             _list;
+
+
+    public:
+        Algo0x13();
+        virtual ~Algo0x13();
+
+    public:
+        static void             _callback(unsigned char *data, size_t len, void *arg);
+
+    public:
+        virtual BandPlane*      compress(const Request& request, 
+                                    unsigned char *data, unsigned long width,
+                                    unsigned long height);
+};
+
+#endif /* DISABLE_JBIG */
+
+#endif /* _ALGO0X13_H_ */
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 smarttab tw=80 cin enc=utf8: */
+
